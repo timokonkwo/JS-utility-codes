d45647b1a72aa34da204031560ce546e279b7fdc
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-08-30 23:20:43 +0100

    added header shadow on scroll utility

---
+++ header shadow on scroll.js
@@ -0,0 +1,7 @@
+function scrollHeader(){
+    const header = document.getElementById('header');
+    // when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
